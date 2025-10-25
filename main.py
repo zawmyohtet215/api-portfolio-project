319bc62f1c893f2fffcec7a09a3e006d34278da4
zawmyohtet215 <61734517+zawmyohtet215@users.noreply.github.com>
2025-10-25 07:22:05 +0000

    4th coomit of ZMH

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 """FastAPI program"""
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
 from datetime import date
 
 import crud, schemas
 from database import SessionLocal
 
-app = FastAPI()
+api_description = """
+This API provides read-only access to info from the SportsWorldCentral(SWC) Fantasy Football API.
+The endpoints are grouped into the following categories:
+
+## Analytics
+Gen information about the health of the API and counts of leagues, teams, and players.
+
+## Player
+You can get a list of NFL players, or search for an individual player by player_id.
+
+## Scoring
+You can get a list of NFL player performances, including the fantasy points they scored using 
+SWC league scoring.
+
+## Membership
+Get information about all the SWC fantasy football leagues and the teams in them.
+"""
+
+#FastAPI constructor with additional details added for OpenAPI Specification
+app = FastAPI(description=api_description,
+            title="Sports World Central (SWC) Fantasy Football API -ZMH",
+            version="0.1")
 
 # Dependency
 def get_db():
@@ -22,11 +43,11 @@ async def root():
     return {"message": "API health check successful"}
 
 @app.get("/v0/players/", response_model=list[schemas.Player])
-def read_players(skip: int = 0,
-                limit: int = 100,
-                minimum_last_changed_date: date = None,
-                first_name: str = None,
-                last_name: str=None,
+def read_players(skip: int = Query(0, description="The number of items to skip at the beginning of API call."),
+                limit: int = Query(100, description="The number of records to return after the skipped records."),
+                minimum_last_changed_date: date = Query(None, description="The minimum date of change that you want to run records. Exclude any records changed before this."),
+                first_name: str = Query(None, description="The first name of the players to return."),
+                last_name: str=Query(None,description="The last name of the players to return."),
                 db: Session = Depends(get_db)
                 ):
 
@@ -39,7 +60,13 @@ def read_players(skip: int = 0,
                                 )
     return players
 
-@app.get("/v0/players/{player_id}", response_model=schemas.Player)
+@app.get("/v0/players/{player_id}", 
+            response_model=schemas.Player,
+            summary="Get one player using the Player ID, which is internal to SWC",
+            description="If you have an SWC Player ID of a player from another API call such as v0_get_players, you can call this API using the player ID",
+            response_description="One NFL player",
+            operation_id="v0_get_players_by_player_id",
+            tags=["player"])
 def read_player(player_id: int,
                 db: Session = Depends(get_db)):
     player = crud.get_player(db, player_id=player_id)
